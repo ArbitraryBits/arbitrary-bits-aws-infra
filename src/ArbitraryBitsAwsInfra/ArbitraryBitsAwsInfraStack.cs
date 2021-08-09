11fdfd875db25915ba707c7e1f52d8f875b3c35d
Anton Efimenko <reloni@ya.ru>
2021-08-09 22:18:53 +0300

    Add example VPC

--- src/ArbitraryBitsAwsInfra/ArbitraryBitsAwsInfraStack.cs
+++ src/ArbitraryBitsAwsInfra/ArbitraryBitsAwsInfraStack.cs
@@ -1,4 +1,5 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
 
 namespace ArbitraryBitsAwsInfra
 {
@@ -6,7 +7,18 @@ namespace ArbitraryBitsAwsInfra
     {
         internal ArbitraryBitsAwsInfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var vpc = new Vpc(this, "ExampleVPC", new VpcProps {
+                Cidr = "10.0.0.0/16",
+                MaxAzs = 1,
+                NatGateways = 0,
+                SubnetConfiguration = new [] {
+                    new SubnetConfiguration() {
+                        Name = "Private subnet",
+                        SubnetType = SubnetType.PUBLIC,
+                        CidrMask = 24
+                    }
+                }
+            });
         }
     }
 }
