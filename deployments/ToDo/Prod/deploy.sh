5b70fbcf14b58c6274faa8f6f013fc3f24e14f33
Anton Efimenko <reloni@ya.ru>
2022-01-14 22:19:54 +0300

    Update todo deploy script

--- deployments/ToDo/Prod/deploy.sh
+++ deployments/ToDo/Prod/deploy.sh
@@ -5,4 +5,5 @@ SECRETS_BUCKET_REGION=us-east-1
 SECRETS_BUCKET_NAME=task-manager-setup-secrets
 SECRETS_FILE_NAME=task-manager-service-rds-prod.txt
 aws s3 --region ${SECRETS_BUCKET_REGION} cp s3://${SECRETS_BUCKET_NAME}/${SECRETS_FILE_NAME} - > .env && \
-    docker compose up -d
+    docker compose up -d && \
+    rm .env
